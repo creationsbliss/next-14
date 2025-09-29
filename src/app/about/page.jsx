e60d2bc245818425a65e87cfd0e34eb71159183c
Musfiq Rahman <procreationsbliss@gmail.com>
2025-09-29 15:01:39 +0600

    Add about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,31 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
 const AboutPage = () => {
-  return <div>About page</div>;
+  return (
+    <div className="flex gap-8 mt-10">
+      <div className="flex-1 gap-8 flex flex-col">
+        <h1 className="font-semibold text-2xl"> About Page </h1>
+        <p>
+          lorem is a dummy text. lorem is a dummy text. lorem is a dummy text.
+          lorem is a dummy text. lorem is a dummy text. lorem is a dummy text.
+          lorem is a dummy text. lorem is a dummy text. lorem is a dummy text
+        </p>
+        <div className="flex gap-4">
+          <Button className="min-w-[80px] px-4 py-4"> Read </Button>
+          <Button className="min-w-[80px] px-4 py-4"> Learn </Button>
+        </div>
+      </div>
+      <div className="relative flex-1">
+        <Image
+          src="/about.png"
+          alt="about image"
+          className="object-contain"
+          fill
+        />
+      </div>
+    </div>
+  );
 };
 
 export default AboutPage;
