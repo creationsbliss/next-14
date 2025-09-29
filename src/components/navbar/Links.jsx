b302a60bcdc090d631408b76a576d8f4f27d2a80
Musfiq Rahman <procreationsbliss@gmail.com>
2025-09-29 13:51:38 +0600

    Add responsive menu

--- src/components/navbar/Links.jsx
+++ src/components/navbar/Links.jsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { MenuIcon } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 import { cn } from "@/lib/utils";
 import { Button } from "../ui/button";
 
@@ -38,39 +40,84 @@ const isAdmin = true;
 
 const Links = () => {
   const pathname = usePathname();
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="flex gap-8">
-      {links.map((link) => (
-        <Link
-          key={link.title}
-          href={link.path}
-          className={cn(pathname === link.path && "underline")}
-        >
-          {link.title}
-        </Link>
-      ))}
-      {session ? (
-        <>
-          {isAdmin && (
+    <>
+      <div className="hidden lg:block">
+        <div className="flex gap-8">
+          {links.map((link) => (
             <Link
-              href="/admin"
-              className={cn(pathname === "/admin" && "underline")}
+              key={link.title}
+              href={link.path}
+              className={cn(pathname === link.path && "underline")}
             >
-              Admin
+              {link.title}
+            </Link>
+          ))}
+          {session ? (
+            <>
+              {isAdmin && (
+                <Link
+                  href="/admin"
+                  className={cn(pathname === "/admin" && "underline")}
+                >
+                  Admin
+                </Link>
+              )}
+              <Button>Logout</Button>
+            </>
+          ) : (
+            <Link
+              href="/login"
+              className={cn(pathname === "/login" && "underline")}
+            >
+              Login
             </Link>
           )}
-          <Button>Logout</Button>
-        </>
-      ) : (
-        <Link
-          href="/login"
-          className={cn(pathname === "/login" && "underline")}
-        >
-          Login
-        </Link>
-      )}
-    </div>
+        </div>
+      </div>
+
+      <div>
+        <MenuIcon
+          className="block lg:hidden"
+          onClick={() => setIsOpen((prev) => !prev)}
+        />
+        {isOpen && (
+          <div className="flex flex-col items-start gap-8 mt-8 block lg:hidden">
+            {links.map((link) => (
+              <Link
+                key={link.title}
+                href={link.path}
+                className={cn(pathname === link.path && "underline")}
+              >
+                {link.title}
+              </Link>
+            ))}
+            {session ? (
+              <>
+                {isAdmin && (
+                  <Link
+                    href="/admin"
+                    className={cn(pathname === "/admin" && "underline")}
+                  >
+                    Admin
+                  </Link>
+                )}
+                <Button>Logout</Button>
+              </>
+            ) : (
+              <Link
+                href="/login"
+                className={cn(pathname === "/login" && "underline")}
+              >
+                Login
+              </Link>
+            )}
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
